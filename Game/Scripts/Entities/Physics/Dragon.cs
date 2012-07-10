d312c2874d6a797d1bf1d7e0facc71b22dbe3eba
Filip Lundgren <filip@inkdev.net>
2012-07-10 23:32:53 +0200

    Updated to latest CryMono build, brings rough animation support.

---
+++ Game/Scripts/Entities/Physics/Dragon.cs
@@ -0,0 +1,18 @@
+﻿using CryEngine;
+
+namespace CryGameCode.Entities.Physics
+{
+	[Entity(Category = "Samples")]
+	public class Dragon : Entity
+	{
+		public override void OnSpawn()
+		{
+			LoadObject("Objects/characters/Dragon/Dragon.cdf");
+		}
+
+		protected override void OnReset(bool enteringGame)
+		{
+			PlayAnimation("2landing");
+		}
+	}
+}
