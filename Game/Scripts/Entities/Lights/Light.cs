8f710a4cab55be6d26e06bac5f74f2915d0ec978
Filip Lundgren <filip@inkdev.net>
2012-09-02 15:00:45 +0200

    Updated to latest CryMono build, added sample Light entity.

--- Game/Scripts/Entities/Lights/Light.cs
+++ Game/Scripts/Entities/Lights/Light.cs
@@ -2,24 +2,87 @@
 
 namespace CryGameCode.Entities.Lights
 {
-	[Entity(Category = "Lights")]
+	[Entity(Category = "Lights", Icon="Light.bmp")]
 	public class Light : Entity
 	{
-		public override void OnSpawn()
-		{
-			LightSource = new LightSource(Position);
+		const int LightSlot = 1;
 
-			LightSource.Color = Color.Red;
-			LightSource.HDRDynamic = 10;
-			LightSource.Radius = 10.0f;
+		protected override void OnInit()
+		{
+			Flags |= EntityFlags.ClientOnly;
+			OnReset(true);
 		}
 
-		protected override void OnMove()
+		protected override void OnReset(bool enteringGame)
 		{
-			LightSource.Origin = Transform.GetTranslation();
-			LightSource.Transform = Transform;
+			Activate(Activated);
 		}
 
-		LightSource LightSource { get; set; }
+		protected override void OnPropertyChanged(string propertyName, EntityPropertyType propertyType, string valueString)
+		{
+			Activate(Activated, true);
+		}
+
+		public void Activate(bool activate, bool force = false)
+		{
+			if ((activate && !Activated) || force)
+			{
+				Activated = true;
+
+				var lightParams = new LightParams();
+				lightParams.lightStyle = LightStyle;
+				lightParams.origin = Vec3.Zero;
+				lightParams.lightFrustumAngle = 45;
+				lightParams.radius = Radius;
+				lightParams.coronaScale = CoronaScale;
+				lightParams.coronaIntensity = CoronaIntensity;
+				lightParams.coronaDistSizeFactor = CoronaDistSizeFactor;
+				lightParams.coronaDistIntensityFactor = CoronaDistIntensityFactor;
+
+				lightParams.color = DiffuseColor;
+				lightParams.specularMultiplier = SpecularMultiplier;
+
+				lightParams.hdrDynamic = HDRDynamic;
+
+				LoadLight(lightParams, LightSlot);
+			}
+			else
+			{
+				Activated = false;
+				FreeSlot(LightSlot);
+			}
+		}
+
+		[EditorProperty]
+		public bool Activated { get; set; }
+
+		[EditorProperty(DefaultValue = 10)]
+		public float Radius;
+
+		#region Style
+		[EditorProperty]
+		public int LightStyle;
+
+		[EditorProperty(DefaultValue = 1)]
+		public float CoronaScale;
+		[EditorProperty(DefaultValue = 1)]
+		public float CoronaIntensity;
+		[EditorProperty(DefaultValue = 1)]
+		public float CoronaDistSizeFactor;
+		[EditorProperty(DefaultValue = 1)]
+		public float CoronaDistIntensityFactor;
+		#endregion
+
+		#region Color
+		[EditorProperty(Type = EntityPropertyType.Color)]
+		public Vec3 DiffuseColor { get; set; }
+		[EditorProperty(DefaultValue = 1)]
+		public float DiffuseMultiplier;
+		[EditorProperty(DefaultValue = 1)]
+		public float SpecularMultiplier;
+
+		[EditorProperty]
+		public float HDRDynamic;
+		#endregion
 	}
 }
