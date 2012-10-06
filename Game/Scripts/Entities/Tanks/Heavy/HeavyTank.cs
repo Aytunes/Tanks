f97ccad0b19f43b773c931ed2340c127495beedd
Filip Lundgren <filip@inkdev.net>
2012-10-06 17:54:40 +0200

    HeavyTank rocket: Fire from turret_term

--- Game/Scripts/Entities/Tanks/Heavy/HeavyTank.cs
+++ Game/Scripts/Entities/Tanks/Heavy/HeavyTank.cs
@@ -15,7 +15,10 @@ namespace CryGameCode.Tanks
 
 		public override void FireWeapon(Vec3 mouseWorldPos)
 		{
-			var rocket = Entity.Spawn<Rocket>("1337rocket", Turret.Position, Turret.Rotation);
+			var jointAbsolute = Turret.GetJointAbsolute("turret_term");
+			jointAbsolute.T = Turret.Transform.TransformPoint(jointAbsolute.T);
+
+			var rocket = Entity.Spawn<Rocket>("1337rocket", jointAbsolute.T, Turret.Rotation);
 		}
 	}
 }
