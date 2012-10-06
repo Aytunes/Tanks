050366fc556798022d675d25b094f136b8ca39fe
Filip Lundgren <filip@inkdev.net>
2012-10-06 18:19:41 +0200

    Rocket: Spawn explosion particle on impact

--- Game/Scripts/Entities/Projectiles/Rocket.cs
+++ Game/Scripts/Entities/Projectiles/Rocket.cs
@@ -29,6 +29,9 @@ namespace CryGameCode.Projectiles
 
 		protected override void OnCollision(EntityId targetEntityId, Vec3 hitPos, Vec3 dir, short materialId, Vec3 contactNormal)
 		{
+			var effect = ParticleEffect.Get("explosions.explosive_bullet.default");
+			effect.Spawn(hitPos);
+
 			Remove();
 		}
 
