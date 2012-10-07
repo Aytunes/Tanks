e275307cf98387c722dfdd0c3e9df82bd250fc16
Filip Lundgren <filip@inkdev.net>
2012-10-07 15:53:00 +0200

    Rocket: use impulse instead of changing velocity

--- Game/Scripts/Entities/Projectiles/Rocket.cs
+++ Game/Scripts/Entities/Projectiles/Rocket.cs
@@ -23,7 +23,7 @@ namespace CryGameCode.Projectiles
 			Physics.Mass = Mass;
 
 			TravelDir = Rotation.Column1;
-			Velocity = TravelDir * rocketSpeed;
+			Physics.AddImpulse(TravelDir * rocketSpeed);
 		}
 
 		protected override void OnCollision(EntityId targetEntityId, Vec3 hitPos, Vec3 dir, short materialId, Vec3 contactNormal)
