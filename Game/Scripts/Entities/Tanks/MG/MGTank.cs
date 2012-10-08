6a06e5a998e8e7ae4bb9853aee6098a93c7ad003
Filip Lundgren <filip@inkdev.net>
2012-10-08 02:26:00 +0200

    MGTank: Spawn bullets

--- Game/Scripts/Entities/Tanks/MG/MGTank.cs
+++ Game/Scripts/Entities/Tanks/MG/MGTank.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 using CryEngine;
+using CryGameCode.Projectiles;
 
 namespace CryGameCode.Tanks
 {
@@ -14,7 +15,10 @@ namespace CryGameCode.Tanks
 
 		public override void FireWeapon(Vec3 mouseWorldPos)
 		{
-			throw new NotImplementedException();
+			var jointAbsolute = Turret.GetJointAbsolute("turret_term");
+			jointAbsolute.T = Turret.Transform.TransformPoint(jointAbsolute.T);
+
+			var bullet = Entity.Spawn<Bullet>("awesomeBullet", jointAbsolute.T, Turret.Rotation);
 		}
 	}
 }
