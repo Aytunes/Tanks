3634ed6e87ee3e376d8d7dacdfef6e5a75beb77f
Ruan PA <ruan@inkdev.net>
2012-10-08 21:11:05 +0100

    Changed rocket explosion

--- Game/Scripts/Entities/Projectiles/Rocket.cs
+++ Game/Scripts/Entities/Projectiles/Rocket.cs
@@ -9,7 +9,7 @@ namespace CryGameCode.Projectiles
 		public override string Model { get { return "objects/projectiles/rocket.cgf"; } }
 		public override float Mass { get { return 20; } }
 		public override float Speed { get { return 1290; } }
-		public override string Effect { get { return "explosions.explosive_bullet.default"; } }
+		public override string Effect { get { return "explosions.C4_explosion.c4"; } }
 		public override float Damage { get { return 20; } }
 		public override DamageType DamageType { get { return DamageType.Explosive; } }
 	}
