6c6c669d336ba58c7182126ec233cc20127816d9
Jake Oliver <mystichobo@gmail.com>
2012-11-12 20:39:14 +0000

    Changed Muzzleflashes a bit, fixed MG a bit

--- Game/Scripts/Entities/Tanks/Turrets/MG.cs
+++ Game/Scripts/Entities/Tanks/Turrets/MG.cs
@@ -1,14 +1,29 @@
-﻿
+﻿using System;
+using CryEngine;
+using CryGameCode.Projectiles;
+
 namespace CryGameCode.Tanks
 {
 	public class MGTank : TankTurret
 	{
+	
+		static float impulseStrength = 5;		
+	
 		public MGTank(Tank tank) : base(tank) { }
         private MGTank() { }
+		
+		protected override void OnFire(Vec3 firePos)
+		{
+			var muzzleFlash = ParticleEffect.Get("weapon_fx.tank.tank125.muzzle_flash.muzzle_flash_small");
+			muzzleFlash.Spawn(firePos, Attachment.Rotation.Column1, 0.5f);
+
+			Owner.Physics.AddImpulse(-Attachment.Rotation.Column1 * impulseStrength);
+		}			
 
 		public override string Model { get { return "objects/tanks/turret_mg.chr"; } }
 		public override string LeftHelper { get { return "turret_term_2"; } }
 		public override string RightHelper { get { return "turret_term_1"; } }
+		public override Type ProjectileType { get { return typeof(MGBullet); } }		
 		public override float TimeBetweenShots { get { return 0.1f; } }
 		public override bool AutomaticFire { get { return true; } }
 	}
