c95233243e4911baacb28bbbe6d0d99530ec92f3
Filip Lundgren <filip@inkdev.net>
2012-11-14 03:36:07 +0100

    New tank movement courtesy of quick

--- Game/Scripts/Entities/Tanks/TankMovement.cs
+++ Game/Scripts/Entities/Tanks/TankMovement.cs
@@ -2,82 +2,107 @@
 
 namespace CryGameCode.Tanks
 {
-	public partial class Tank
-	{
-		protected override void OnPrePhysicsUpdate()
-		{
-			if(IsDestroyed)
-				return;
+    public partial class Tank
+    {
 
-			var moveRequest = new EntityMovementRequest();
-			moveRequest.type = EntityMoveType.Normal;
 
-			if(!Physics.LivingStatus.IsFlying)
-				moveRequest.velocity = VelocityRequest;
+        private float movementDamping = 50f;
+        private float rotationDamping = 350f;
 
-			moveRequest.rotation = LocalRotation;
-			moveRequest.rotation.SetRotationXYZ(RotationRequest * Time.DeltaTime);
-			moveRequest.rotation = moveRequest.rotation.Normalized;
 
-			AddMovement(ref moveRequest);
+        protected override void OnPrePhysicsUpdate()
+        {
+            if (IsDestroyed)
+                return;
 
-			VelocityRequest = Vec3.Zero;
-			RotationRequest = Vec3.Zero;
+            var moveRequest = new EntityMovementRequest();
+            moveRequest.type = EntityMoveType.Normal;
 
-			if(moveRequest.velocity != Vec3.Zero)
-			{
-				var moveMat = Material.Find("objects/tanks/tracksmoving");
-				if(moveMat != null && !m_leftTrack.IsDestroyed && !m_rightTrack.IsDestroyed)
-				{
-					m_leftTrack.Material = moveMat;
-					m_rightTrack.Material = moveMat;
-				}
-			}
-			else
-			{
-				var defaultMat = Material.Find("objects/tanks/tracks");
-				if(defaultMat != null && !m_leftTrack.IsDestroyed && !m_rightTrack.IsDestroyed)
-				{
-					m_leftTrack.Material = defaultMat;
-					m_rightTrack.Material = defaultMat;
-				}
-			}
-		}
+            // dampen the movement. 
+            MathHelpers.Interpolate(ref m_rotation, 0, rotationDamping * Time.DeltaTime);
+            MathHelpers.Interpolate(ref m_acceleration, 0, movementDamping * Time.DeltaTime);
 
-		private void OnMoveRight(ActionMapEventArgs e)
-		{
-			RotationRequest.Z -= tankTurnSpeed;
-		}
 
-		private void OnMoveLeft(ActionMapEventArgs e)
-		{
-			RotationRequest.Z += tankTurnSpeed;
-		}
+            RotationRequest = new Vec3(0, 0, m_rotation);
 
-		private void OnMoveForward(ActionMapEventArgs e)
-		{
-			VelocityRequest += LocalRotation.Column1 * TankSpeed * SpeedMultiplier;
-		}
+            VelocityRequest = LocalRotation.Column1 * m_acceleration * SpeedMultiplier;
 
-		private void OnMoveBack(ActionMapEventArgs e)
-		{
-			VelocityRequest += LocalRotation.Column1 * -TankSpeed * SpeedMultiplier;
-		}
+            if (!Physics.LivingStatus.IsFlying)
+                moveRequest.velocity = VelocityRequest;
 
-		private void OnSprint(ActionMapEventArgs e)
-		{
-			if(e.KeyEvent == KeyEvent.OnPress)
-				SpeedMultiplier = 1.5f;
-			else if(e.KeyEvent == KeyEvent.OnRelease)
-				SpeedMultiplier = 1;
-		}
+            moveRequest.rotation = LocalRotation;
+            moveRequest.rotation.SetRotationXYZ(RotationRequest * Time.DeltaTime);
+            moveRequest.rotation = moveRequest.rotation.Normalized;
 
-		protected Vec3 VelocityRequest;
-		protected Vec3 RotationRequest;
+            AddMovement(ref moveRequest);
 
-		static float tankTurnSpeed = 2;
+            // reset movement vectors
+            VelocityRequest = Vec3.Zero;
+            RotationRequest = Vec3.Zero;
 
-		public float SpeedMultiplier = 1.0f;
-		public virtual float TankSpeed { get { return 10; } }
-	}
+            if (moveRequest.velocity != Vec3.Zero)
+            {
+                var moveMat = Material.Find("objects/tanks/tracksmoving");
+                if (moveMat != null && !m_leftTrack.IsDestroyed && !m_rightTrack.IsDestroyed)
+                {
+                    m_leftTrack.Material = moveMat;
+                    m_rightTrack.Material = moveMat;
+                }
+            }
+            else
+            {
+                var defaultMat = Material.Find("objects/tanks/tracks");
+                if (defaultMat != null && !m_leftTrack.IsDestroyed && !m_rightTrack.IsDestroyed)
+                {
+                    m_leftTrack.Material = defaultMat;
+                    m_rightTrack.Material = defaultMat;
+                }
+            }
+        }
+
+
+        private void OnMoveRight(ActionMapEventArgs e)
+        {
+            m_rotation = MathHelpers.Clamp(m_rotation - RotationSpeed * Time.DeltaTime, -m_maxRotationSpeed, m_maxRotationSpeed);
+        }
+
+        private void OnMoveLeft(ActionMapEventArgs e)
+        {
+            m_rotation = MathHelpers.Clamp(m_rotation + RotationSpeed * Time.DeltaTime, -m_maxRotationSpeed, m_maxRotationSpeed);
+        }
+
+        private void OnMoveForward(ActionMapEventArgs e)
+        {
+            m_acceleration = MathHelpers.Clamp(m_acceleration + TankSpeed * Time.DeltaTime, -m_maxSpeed, m_maxSpeed);
+
+        }
+
+        private void OnMoveBack(ActionMapEventArgs e)
+        {
+            m_acceleration = MathHelpers.Clamp(m_acceleration - TankSpeed * Time.DeltaTime, -m_maxSpeed, m_maxSpeed);
+        }
+
+        private void OnSprint(ActionMapEventArgs e)
+        {
+            if (e.KeyEvent == KeyEvent.OnPress)
+                SpeedMultiplier = 1.5f;
+            else if (e.KeyEvent == KeyEvent.OnRelease)
+                SpeedMultiplier = 1;
+        }
+
+
+        private float m_acceleration;
+        private float m_rotation;
+        private const float m_maxSpeed = 8f;
+        private const float m_maxRotationSpeed = 1f;
+
+        protected Vec3 VelocityRequest;
+        protected Vec3 RotationRequest;
+
+        static float tankTurnSpeed = 2;
+
+        public float SpeedMultiplier = 1.0f;
+        public virtual float TankSpeed { get { return 24f; } }
+        public virtual float RotationSpeed { get { return 10f; } }
+    }
 }
