2e5a6e70b97a3f11b6da4b04d9c59b317039a981
Filip Lundgren <filip@inkdev.net>
2013-01-02 20:42:22 +0100

    Updated to latest CryMono build.

---
+++ Game/Scripts/Entities/MyEntity.cs
@@ -0,0 +1,18 @@
+﻿using CryEngine;
+
+namespace CryGameCode.Entities
+{
+    [Entity(Category = "MyCategory")]
+    public class MyEntity : Entity
+    {
+        protected override void OnEditorReset(bool enteringGame)
+        {
+            LoadObject(Model);
+
+            PlayAnimation("Default", AnimationFlags.Loop);
+        }
+
+        [EditorProperty]
+        public string Model { get; set; }
+    }
+}
