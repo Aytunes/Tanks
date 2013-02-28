f7d3ff30404f228321cfe49c9cb94d580a30ee99
Ruan PA <ruan@inkdev.net>
2013-02-28 20:00:50 +0100

    Fixed movement framerate independence

--- Game/Scripts/Entities/Tanks/TankMovement.cs
+++ Game/Scripts/Entities/Tanks/TankMovement.cs
@@ -60,7 +60,7 @@ namespace CryGameCode.Tanks
 
             var dragDeceleration = (1.2f * 0.588f * 1.27f * (normalizedVelocity * (float)Math.Pow(prevVelocity.Length, 2))) / Physics.Status.Dynamics.Mass;
 
-            moveRequest.velocity = prevVelocity + (forwardAcceleration - frictionDeceleration - dragDeceleration) * Time.DeltaTime;
+            moveRequest.velocity = prevVelocity + (forwardAcceleration - frictionDeceleration - dragDeceleration);
             moveRequest.velocity.ClampLength(GameCVars.tank_movementMaxSpeed);
 
             ///////////////////////////
