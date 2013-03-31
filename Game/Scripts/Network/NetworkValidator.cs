68b93968e965e78b394b7e9c9a103adeaa1162d5
Ruan PA <ruan@inkdev.net>
2013-03-31 18:54:10 +0200

    Added new NetworkValidator class for sanity checks

---
+++ Game/Scripts/Network/NetworkValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CryEngine;
+
+namespace CryGameCode.Network
+{
+	public static class NetworkValidator
+	{
+		public static void Server(string reason = null)
+		{
+			if (!Game.IsServer)
+				throw new NetworkValidationException(string.Format("Server access level denied: {0}", reason ?? "no reason given"));
+		}
+	}
+
+	public class NetworkValidationException : Exception
+	{
+		public NetworkValidationException()
+		{
+		}
+
+		public NetworkValidationException(string message)
+			: base(message)
+		{
+		}
+
+		public NetworkValidationException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
